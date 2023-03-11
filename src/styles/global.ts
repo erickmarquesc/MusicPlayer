1596a9d3620a6bd9d89e53e1c85b7ec6b2a9db6a
Erick Marques <erick.marquesc@hotmail.com>
2023-03-11 18:21:14 -0300

    style: :lipstick: create a globalstyles

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  body {
+    background-color: #0F0D13;
+  }
+  
+  body * {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
