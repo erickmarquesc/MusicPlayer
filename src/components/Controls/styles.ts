81ce1641ba2992494ea801bbd53feefd3466d1f5
Erick Marques <erick.marquesc@hotmail.com>
2023-03-12 18:54:55 -0300

    feat: :sparkles: create a new component

--- src/components/Controls/styles.ts
+++ src/components/Controls/styles.ts
@@ -1,7 +1,29 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const ControlsContainer = styled.div`
+interface ControlsContainerProps{
+  variant: 'primary' | 'secundary';
+};
+
+export const ControlsContainer = styled.div<ControlsContainerProps>`
   display: flex;
-  justify-content: space-between;
   margin-top: 28px;
+  justify-content: space-between;
+
+  ${(props) => props.variant === 'primary' ? css`
+    padding-inline: 58px;
+  `: props.variant === 'secundary' && css``}
+
+  img{
+    width: 30px;
+    height: 30px;
+
+
+    :hover{
+      cursor: pointer;
+    };
+  };
+
+  @media (max-width: 670px) {
+    padding-inline: 30px;
+  };
 `;
